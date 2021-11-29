93b5b14c9e8b45151aad97077bca514e39095b1e
olaza003 <olaza003@ucr.edu>
2021-11-28 21:15:41 -0800

    finish part 2

--- spamdel.sh
+++ spamdel.sh
@@ -10,7 +10,41 @@ grep -n "$key" $File | awk -F":" '{print $1}' > "$hold"
 
 while read -r line; do
 last=$(tail -n 1 "$hold")
-echo "last is $last" 
+#echo "last is $last" 
+echo $last | mail >> /root/cs183-Project/spam.txt
 echo "d $last" | mail #deletes the mail according to whats the last line of the holder.txt
 sed -i '$d' "$hold" #now it deletes the last line of the hold
 done < "$hold"
+
+#2nd part
+
+mailx -H | awk '{print $8}' > "$File"
+len=$(grep -c ^ $File)
+mailnum=0 
+
+while [ $mailnum -lt $len ]; do
+mailnum=$(( mailnum+1 ))
+echo " " > $hold
+echo $mailnum | mail > $File
+i=0 #0-false; 1-true
+x=0 #counter
+num=$(grep -c ^ $File)
+num=$(( num-6 ))
+	while read -r line; do
+	   x=$(( x+1 ))
+	   if [ $i == 0 ]; then 
+		if [ $x -gt $num ]
+		then
+		 i=1
+		fi
+	   else
+		echo "$line" >> $hold
+	   fi
+	done<"$File"
+
+if grep -q $key /root/cs183-Project/holder.txt
+then
+  echo $mailnum | mail >> /root/cs183-Project/spam.txt
+  echo "d $mailnum" | mail
+fi
+done
