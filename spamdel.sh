79488cf0fd66b64f65ed977ba2c99d7db4654079
olaza003 <olaza003@ucr.edu>
2021-11-28 18:43:44 -0800

    1st part of spam deletion

--- spamdel.sh
+++ spamdel.sh
@@ -8,3 +8,9 @@ mailx -H | awk '{print $8, $9, $10, $11}' > "$File"
 #after the subject stored we check if those are keywords
 grep -n "$key" $File | awk -F":" '{print $1}' > "$hold"
 
+while read -r line; do
+last=$(tail -n 1 "$hold")
+echo "last is $last" 
+echo "d $last" | mail #deletes the mail according to whats the last line of the holder.txt
+sed -i '$d' "$hold" #now it deletes the last line of the hold
+done < "$hold"
